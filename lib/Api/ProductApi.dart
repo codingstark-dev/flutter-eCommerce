4ce9c8c7d6682a92160622f269129dadf69494eb
Himanshu Maurya <mhimanshu.338@gmail.com>
2019-12-05 01:45:19 +0530

    constructors

--- lib/Api/ProductApi.dart
+++ lib/Api/ProductApi.dart
@@ -1,7 +1,11 @@
 class ProductApi {
   final String productName;
+  final String productPrice;
+  final String productDescription;
+  final String productColor;
+  final String productSize;
+  final String productImages;
 
-  ProductApi(this.productName);
-
-  
+  ProductApi(this.productName, this.productPrice, this.productDescription,
+      this.productColor, this.productSize, this.productImages);
 }
